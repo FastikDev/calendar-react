98c8fca62baa99faa5cc39ca08331f085bb02710
AlinkaSuper <sir.fastik18@gmail.com>
2024-08-21 23:42:33 +0300

    header month logic

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,70 @@
-import React from 'react';
-
-import './header.scss';
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import { generateWeekRange, getWeekStartDate } from "../../utils/dateUtils";
+import "./header.scss";
 
 const Header = () => {
+  const [displayedMonth, setDispalayedMoth] = useState("");
+  const [currentDate, setCurrenDate] = useState(new Date());
+
+  const updateDisplayedMonth = (date) => {
+    const startOfWeek = getWeekStartDate(date);
+    const weekRange = generateWeekRange(startOfWeek);
+
+    const startMonth = moment(weekRange[0]).format("MMM");
+    const endMonth = moment(weekRange[6]).format("MMM");
+
+    setDispalayedMoth(
+      startMonth === endMonth ? startMonth : `${startMonth} - ${endMonth}`
+    );
+  };
+
+  useEffect(() => {
+    updateDisplayedMonth(currentDate);
+  }, [currentDate]);
+
+  const handlePrevWeek = () => {
+    setCurrenDate((prevDate) => {
+      const newDate = new Date(prevDate);
+      newDate.setDate(prevDate.getDate() - 7);
+      return newDate;
+    });
+  };
+
+  const handleNextWeek = () => {
+    setCurrenDate((prevDate) => {
+      const newDate = new Date(prevDate);
+      newDate.setDate(prevDate.getDate() + 7);
+      return newDate;
+    });
+  };
+
+  const handleToday = () => {
+    setCurrenDate(new Date());
+  };
+
   return (
     <header className="header">
       <button className="button create-event-btn">
         <i className="fas fa-plus create-event-btn__icon"></i>Create
       </button>
       <div className="navigation">
-        <button className="navigation__today-btn button">Today</button>
-        <button className="icon-button navigation__nav-icon">
+        <button className="navigation__today-btn button" onClick={handleToday}>
+          Today
+        </button>
+        <button
+          className="icon-button navigation__nav-icon"
+          onClick={handlePrevWeek}
+        >
           <i className="fas fa-chevron-left"></i>
         </button>
-        <button className="icon-button navigation__nav-icon">
+        <button
+          className="icon-button navigation__nav-icon"
+          onClick={handleNextWeek}
+        >
           <i className="fas fa-chevron-right"></i>
         </button>
-        <span className="navigation__displayed-month"></span>
+        <span className="navigation__displayed-month">{displayedMonth}</span>
       </div>
     </header>
   );
