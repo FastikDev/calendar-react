b48e9db0c525af181c58090f9811adde704c889e
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-08-24 07:18:52 +0300

    Update Day.jsx

--- src/components/day/Day.jsx
+++ src/components/day/Day.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hour from "../hour/Hour";
 
-const Day = ({ dataDay, dayEvents }) => {
+const Day = ({ dataDay, dayEvents, setEvents, month }) => {
   const hours = Array(24)
     .fill()
     .map((val, index) => index);
@@ -9,13 +9,20 @@ const Day = ({ dataDay, dayEvents }) => {
   return (
     <div className="calendar__day" data-day={dataDay}>
       {hours.map((hour) => {
-        //getting all events from the day we will render
+        // Получаем все события для текущего часа
         const hourEvents = dayEvents.filter(
           (event) => event.dateFrom.getHours() === hour
         );
 
         return (
-          <Hour key={dataDay + hour} dataHour={hour} hourEvents={hourEvents} />
+          <Hour
+            key={`${dataDay}-${hour}`}
+            dataHour={hour}
+            hourEvents={hourEvents} // Массив событий для часа передается в Hour через пропс hourEvents
+            setEvents={setEvents}
+            dataDay={dataDay}
+            month={month}
+          />
         );
       })}
     </div>
