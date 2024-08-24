61e570a1292d61729d4fbc97b6acc474b5767d13
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-08-24 07:21:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Header from "./components/header/Header.jsx";
 import Calendar from "./components/calendar/Calendar.jsx";
 import { getWeekStartDate, generateWeekRange } from "../src/utils/dateUtils.js";
 import "./common.scss";
+import testEvents from "./gateway/events.js";
 
 const App = () => {
   const [weekStartDate, setWeekStartDate] = useState(new Date());
+  const [events, setEvents] = useState(testEvents); // Здесь массив testEvents сохраняется в состоянии
 
   const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
 
@@ -15,7 +17,11 @@ const App = () => {
         weekStartDate={weekStartDate}
         setWeekStartDate={setWeekStartDate}
       />
-      <Calendar weekDates={weekDates} />
+      <Calendar
+        weekDates={weekDates}
+        events={events} // Массив testEvents передается в Calendar через пропс events
+        setEvents={setEvents}
+      />
     </>
   );
 };
